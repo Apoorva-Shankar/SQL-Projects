7f8f44c176e85dfea058212b093c76ab6a613f15
Apoorva Shankar <128100204+Apoorva-Shankar@users.noreply.github.com>
2023-07-06 20:31:27 -0400

    Update and rename HW2 Solutions.sql to Yelp Reviews dataset.sql

---
+++ Yelp Reviews dataset.sql
@@ -0,0 +1,122 @@
+
+
+-- Pizza Restaurants
+
+SELECT * 
+FROM business 
+LIMIT 10;
+
+SELECT
+	COUNT(*)/(
+				SELECT 
+					COUNT(*) 
+				FROM 
+					business 
+				WHERE UPPER(CATEGORIES) LIKE '%PIZZA%'
+			)
+    
+FROM
+	business
+WHERE
+	UPPER(CATEGORIES) LIKE '%PIZZA%'
+AND
+	LENGTH(REPLACE(CATEGORIES, ',', 'AA')) - LENGTH(CATEGORIES) + 1 <= 3;
+
+
+SELECT
+	COUNT(DISTINCT CATEGORIES) AS UNIQUE_CATEGORIES
+FROM
+	business
+WHERE
+	UPPER(CATEGORIES) LIKE '%PIZZA%'
+    AND UPPER(CATEGORIES) LIKE '%RESTAURANTS%'
+    AND UPPER(CATEGORIES) LIKE '%ITALIAN%';
+
+
+-- Italian Pizza restaurants 
+
+SELECT
+	SUM(CATEGORY_COUNT) AS CATEGORY_COUNT
+FROM
+(
+	SELECT
+		CATEGORIES
+		,COUNT(DISTINCT CATEGORIES) AS CATEGORY_COUNT
+		,AVG(REVIEW_COUNT)
+		,76.3768 AS AVG_REVIEWS_ALL_BIZ
+	FROM
+		business
+	WHERE
+		UPPER(CATEGORIES) LIKE '%PIZZA%'
+		AND UPPER(CATEGORIES) LIKE '%RESTAURANTS%'
+		AND UPPER(CATEGORIES) LIKE '%ITALIAN%'
+	GROUP BY CATEGORIES
+	HAVING AVG(REVIEW_COUNT) > (SELECT
+									AVG(REVIEW_COUNT)
+								FROM
+									business
+								)
+) A;
+
+-- “elite_group” groups by generating subcategories based on how long a user has been yelping calculated relative to 7/29/22.  
+
+SELECT
+	CASE
+		WHEN ELITE IS NULL THEN 'no_elite'
+		ELSE 'yes_elite'
+		END AS elite_group
+	,CASE
+		WHEN datediff(DATE('2022-07-29'),YELPING_SINCE)  <= 1000
+        THEN 'group1'
+        WHEN datediff(DATE('2022-07-29'),YELPING_SINCE)  BETWEEN 1000 AND 2999
+        THEN 'group2'
+        WHEN datediff(DATE('2022-07-29'),YELPING_SINCE)  >= 3000
+        THEN 'group3'
+	END AS tenure
+    ,COUNT(*)
+
+FROM
+	yelp.user
+GROUP BY 
+	ELITE_GROUP
+    ,TENURE
+ORDER BY
+	TENURE
+    ,ELITE_GROUP
+;
+    
+-- Restaurants with 2 words in their names
+SELECT
+	COUNT(DISTINCT NAME) AS COUNT_OF_RES
+    ,count(*)
+FROM
+	(
+    SELECT
+		NAME
+		,LENGTH(NAME)
+		,REPLACE(TRIM(NAME), ' ', '~~' )
+		,POSITION('~~' IN REPLACE(TRIM(NAME), ' ', '~~' )) - 1 AS WORD_LEN1
+		,CHAR_LENGTH(TRIM(NAME)) - POSITION('~~' IN REPLACE(TRIM(NAME), ' ', '~~' )) AS WORD_LEN2
+		,LENGTH(REPLACE(TRIM(NAME), ' ', '~~' )) - LENGTH(NAME)
+	FROM
+		business
+	WHERE
+		LENGTH(NAME) - LENGTH(REPLACE(TRIM(NAME), ' ', '' )) = 1
+	GROUP BY 1
+    ) A
+WHERE
+	WORD_LEN2 > WORD_LEN1;
+    
+
+    SELECT
+		COUNT(*)
+	FROM
+		business
+	WHERE
+    
+		LENGTH(TRIM(NAME)) - LENGTH(REPLACE(TRIM(NAME), ' ', '' )) = 1
+	AND 
+	
+	CHAR_LENGTH(TRIM(NAME)) - POSITION('~~' IN REPLACE(TRIM(NAME), ' ', '~~' ))>POSITION('~~' IN REPLACE(TRIM(NAME), ' ', '~~' )) - 1 ;
+
+
